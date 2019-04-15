03b8e98c6291db1c8e439805ba81ce939ea26f21
Or Menashe <menesh2@gmail.com>
2019-04-15 11:22:51 +0300

    add location fetcher

--- LocationFetcher.ts
+++ LocationFetcher.ts
@@ -1,3 +1,17 @@
-class LocationFetcher {
-    
+import { Geolocation, GeolocationReturnType } from "react-native";
+
+export default class LocationFetcher {
+    successCallback = (location: String) => {};
+
+    getCurrentLocation(success: (location: String) => void) {
+        this.successCallback = success
+        Geolocation.requestAuthorization()
+        Geolocation.getCurrentPosition(this.positionArrived);
+        
+        return;
+    }
+
+    positionArrived(position: GeolocationReturnType) {
+        this.successCallback("arrived")
+    }
 }
--- LocationScreen.tsx
+++ LocationScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import { LocationFetcher } from './LocationFetcher'
 
 class LocationScreen extends React.Component {
     
